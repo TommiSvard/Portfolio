f3653381f5f9507ac6e49b88f7b60a0a8b9087a3
TommiSvard <tommi.svard@hotmail.se>
2018-06-07 20:27:04 +0200

    Removed buttons and added in lastfm API and created some functions for that.

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 from flask import Flask, render_template
 from flask_static_files_chache_invalidator import *
+from lastfm import *
 
 app = Flask(__name__)
 app._static_folder = 'C:\\Users\\Tommi\\Desktop\\Portfolio\\static'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', )
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/skills')
-def skills():
-    return render_template('skills.html')
-
+@app.route('/myLastFm')
+def myLastFm():
+    return currentlyListening()
 
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
-
 
 
 
